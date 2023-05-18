9e82b9001be107e4f2878554a5bf8ae932cf80b1
andreyivashchenko <99690245+andreyivashchenko@users.noreply.github.com>
2023-05-18 16:56:30 +0300

    Добавлен Url сервера, на котором размещен бэк

--- src/utills/Url.ts
+++ src/utills/Url.ts
@@ -1,1 +1,2 @@
-export const baseUrlAuth = "http://localhost:5000/auth";
+// export const baseUrlAuth = "http://localhost:5000/auth";
+export const baseUrlAuth = "https://vk-internship-backend.onrender.com/auth";
